9a1f5e388d1049175e834923438288a3f6e92cab
jitesh-95 <1995sirohis@gmail.com>
2022-05-05 00:18:26 +0530

    navbar & homepage changes

--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,4 @@
-import { stays } from "../component/head.js";
+import { stays, countryCodes } from "../component/head.js";
 let bookingdiv = document.querySelector(".search_booking");
 // bookingdiv.innerHTML = stays();
 
@@ -18,3 +18,23 @@ function staysfun() {
   staysdiv.style.borderBottom = "teal";
 }
 document.querySelector("#stays").addEventListener("click", staysfun);
+
+//country codes
+document.querySelector("#country_codes").innerHTML = countryCodes();
+
+//get the app
+let alerttag = document.querySelector("#alert");
+
+function getApp() {
+  let appbtn = document.querySelector("#app_number").value;
+  if (appbtn.length < 10 || appbtn.length < 10) {
+    return (alerttag.innerHTML =
+      "Sorry, there was an issue on our end. Please wait a moment and try again.");
+  }
+  if (appbtn.length === 10) {
+    alerttag.style.color = "green";
+    return (alerttag.innerHTML =
+      "We’ve sent you a text message with a link to download the app.");
+  }
+}
+document.querySelector(".get_the_app").addEventListener("click", getApp);
